1c4f9a415e5183199689ecf61f4f028d21f83c44
Jeferson Silveira <jefersoncomp@gmail.com>
2022-02-18 16:51:26 -0300

    Armazenando código

---
+++ CallerServerClient/ListarVersoes/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ListarVersoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowPanelOlimpo = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowPanelOlimpoWithServer = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowPanelOlimpo, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowPanelOlimpoWithServer, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(390, 309);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowPanelOlimpo
+            // 
+            this.flowPanelOlimpo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowPanelOlimpo.Location = new System.Drawing.Point(3, 3);
+            this.flowPanelOlimpo.Name = "flowPanelOlimpo";
+            this.flowPanelOlimpo.Size = new System.Drawing.Size(189, 303);
+            this.flowPanelOlimpo.TabIndex = 1;
+            // 
+            // flowPanelOlimpoWithServer
+            // 
+            this.flowPanelOlimpoWithServer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowPanelOlimpoWithServer.Location = new System.Drawing.Point(198, 3);
+            this.flowPanelOlimpoWithServer.Name = "flowPanelOlimpoWithServer";
+            this.flowPanelOlimpoWithServer.Size = new System.Drawing.Size(189, 303);
+            this.flowPanelOlimpoWithServer.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 309);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Olimpo Versão";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowPanelOlimpo;
+        private System.Windows.Forms.FlowLayoutPanel flowPanelOlimpoWithServer;
+    }
+}
+
---
+++ CallerServerClient/ListarVersoes/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListarVersoes
+{
+    public partial class Form1 : Form
+    {
+        string appName = "Cliente";
+        string servidorName = "Servidor";
+        string clientExeAppName = "Olimpo.exe";
+        string servidorExeAppName = "Servidor de Aplicacao Olimpo.exe";
+        string pasta = Application.StartupPath; //o caminho onde vai iniciar a leitura procurando pelos aplicativos. Inicialmente esse aplicativo deve ficar na pasta raiz
+
+        public Form1()
+        {
+            InitializeComponent();
+            //pasta = @"C:\workspace\Dlls";
+            this.ListarVersoes();
+        }
+
+        private void ListarVersoes()
+        {
+            DirectoryInfo d = new DirectoryInfo(pasta);
+            int i =1;
+            foreach (var directory in d.GetDirectories())
+            {
+                Button botaoOlimpo = this.PreparaBotao(i);
+                botaoOlimpo.Tag = Path.Combine(directory.FullName, clientExeAppName);
+                botaoOlimpo.Click += botao_Click;
+                i++;
+                Button botaoOlimpoWithServer = this.PreparaBotao(i);
+                botaoOlimpoWithServer.Tag = Path.Combine(directory.FullName, servidorExeAppName);
+                botaoOlimpoWithServer.Click += botao_Click;
+                i++;
+
+                botaoOlimpo.Text = appName + " - " + directory.Name;
+                botaoOlimpoWithServer.Text = servidorName + " - " + directory.Name;
+
+                this.flowPanelOlimpo.Controls.Add(botaoOlimpo);
+                this.flowPanelOlimpoWithServer.Controls.Add(botaoOlimpoWithServer);
+            }
+        }
+
+        void botao_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            Process.Start(b.Tag.ToString());
+        }
+
+        private Button PreparaBotao(int count)
+        {
+            Button b = new Button();
+            b.Dock = System.Windows.Forms.DockStyle.Top;
+            b.Location = new System.Drawing.Point(3, 3);
+            b.Name = "button" + count;
+            b.Size = new System.Drawing.Size(186, 55);
+            b.TabIndex = 0;
+            b.Text = "button" + count;
+            b.UseVisualStyleBackColor = true;
+
+            return b;
+        }
+
+
+    }
+}
